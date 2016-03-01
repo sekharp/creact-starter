51c3bdc23ce72860ded724bb0ae99dc1a9b862cf
Sekhar Paladugu <sekharp@gmail.com>
2016-03-01 15:11:36 -0700

    skills now deleting! yay

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -8,6 +8,24 @@ var Body = React.createClass({
     $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
   },
 
+  handleDelete(id) {
+    $.ajax({
+      url: `/api/v1/skills/${id}`,
+      type: 'DELETE',
+      success: () => {
+        this.removeSkillFromDOM(id);
+      }
+    });
+  },
+
+  removeSkillFromDOM(id) {
+    var newSkills = this.state.skills.filter((skill) => {
+      return skill.id != id;
+    });
+
+    this.setState({ skills: newSkills });
+  },
+
   handleSubmit(skill) {
     var newState = this.state.skills.concat(skill);
     this.setState({ skills: newState })
@@ -17,7 +35,7 @@ var Body = React.createClass({
     return (
       <div>
         <NewSkill handleSubmit={this.handleSubmit} />
-        <AllSkills skills={this.state.skills} />
+        <AllSkills skills={this.state.skills} handleDelete={this.handleDelete} />
       </div>
     )
   }
